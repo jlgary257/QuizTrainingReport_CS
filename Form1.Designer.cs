be725168ff53ee4de606c0a194cccd2087f8498e
Joel Gary A/L John Santhana Dass <Joel.Santhana@uct.com>
2024-12-10 12:56:31 +0800

    The 1st version of the project with no connection to Survey Monkey

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace QuizTrainingReport
+{
+    partial class ResultPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.dateTimeFrom = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeTo = new System.Windows.Forms.DateTimePicker();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(161, 172);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(782, 315);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblMain
+            // 
+            this.lblMain.AutoSize = true;
+            this.lblMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMain.Location = new System.Drawing.Point(382, 34);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(220, 31);
+            this.lblMain.TabIndex = 1;
+            this.lblMain.Text = "SAP Quiz Report";
+            // 
+            // dateTimeFrom
+            // 
+            this.dateTimeFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeFrom.Location = new System.Drawing.Point(559, 127);
+            this.dateTimeFrom.Name = "dateTimeFrom";
+            this.dateTimeFrom.Size = new System.Drawing.Size(124, 22);
+            this.dateTimeFrom.TabIndex = 2;
+            // 
+            // dateTimeTo
+            // 
+            this.dateTimeTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeTo.Location = new System.Drawing.Point(832, 127);
+            this.dateTimeTo.Name = "dateTimeTo";
+            this.dateTimeTo.Size = new System.Drawing.Size(124, 22);
+            this.dateTimeTo.TabIndex = 3;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(488, 132);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(41, 16);
+            this.lblFrom.TabIndex = 4;
+            this.lblFrom.Text = "From:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(776, 133);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(27, 16);
+            this.lblTo.TabIndex = 5;
+            this.lblTo.Text = "To:";
+            this.lblTo.Click += new System.EventHandler(this.lblTo_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(742, 504);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(87, 34);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(913, 504);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(87, 34);
+            this.btnExport.TabIndex = 7;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // ResultPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 589);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.dateTimeTo);
+            this.Controls.Add(this.dateTimeFrom);
+            this.Controls.Add(this.lblMain);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ResultPage";
+            this.Text = "Result Page";
+            this.Load += new System.EventHandler(this.ResultPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.DateTimePicker dateTimeFrom;
+        private System.Windows.Forms.DateTimePicker dateTimeTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizTrainingReport
+{
+    public partial class ResultPage : Form
+    {
+        public ResultPage()
+        {
+            InitializeComponent();
+        }
+        DataTable table = new DataTable();
+        private void lblTo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultPage_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Name", typeof(String));
+            table.Columns.Add("Age", typeof(int));
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.SelectAll();
+            DataObject cpydata = dataGridView1.GetClipboardContent();
+            if (cpydata != null) Clipboard.SetDataObject(cpydata);
+            Microsoft.Office.Interop.Excel.Application xlapp = new Microsoft.Office.Interop.Excel.Application();
+            xlapp.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook workbook;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet;
+            object miseddata = System.Reflection.Missing.Value;
+            workbook = xlapp.Workbooks.Add(miseddata);
+
+            worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets.get_Item(1);
+            Microsoft.Office.Interop.Excel.Range xlr = (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1];
+            xlr.Select();
+
+            worksheet.PasteSpecial(xlr, Type.Missing, Type.Missing, Type.Missing, true);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            String[] lines = File.ReadAllLines(@"C:\Users\Joel.Santhana\Desktop\csBeginner\basic\exportExcel\table.txt");
+            String[] values;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].Split('/');
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+
+                table.Rows.Add(row);
+            }
+        }
+    }
+}
